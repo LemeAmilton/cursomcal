e479c28fa375cc1503b93c875062b1e9dd3fbd46
Amilton Leme <amilton.lme@uol.com.br>
2019-06-16 17:54:06 -0300

    Cidade/Estado/Endereço

--- src/main/java/com/amiltonleme/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/amiltonleme/cursomc/resources/CategoriaResource.java
@@ -1,4 +1,4 @@
-package com.amiltonleme.cursomc.resources;
+	package com.amiltonleme.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.amiltonleme.cursomc.domain.Categoria;
 import com.amiltonleme.cursomc.services.CategoriaService;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -18,9 +20,9 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find (@PathVariable Integer id) { 
+	public ResponseEntity<?> find (@PathVariable Integer id) throws ObjectNotFoundException { 
 		
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj	);
 	}
 }
